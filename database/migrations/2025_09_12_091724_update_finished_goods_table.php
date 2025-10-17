9ceb934b5f9defb0c485cc71d39d1d4d93c36af6
Muhammad Ayaz <143396617+ayazfazlani@users.noreply.github.com>
2025-10-17 14:15:25 +0500

    refactor: comment out migration methods in finished_goods table for future adjustments

--- database/migrations/2025_09_12_091724_update_finished_goods_table.php
+++ database/migrations/2025_09_12_091724_update_finished_goods_table.php
@@ -9,26 +9,26 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('finished_goods', function (Blueprint $table) {
-            // Rename existing column
-            $table->renameColumn('ovality', 'start_ovality');
+    // public function up(): void
+    // {
+    //     Schema::table('finished_goods', function (Blueprint $table) {
+    //         // Rename existing column
+    //         $table->renameColumn('ovality', 'start_ovality');
 
-            // Add new column
-            $table->decimal('end_ovality', 8, 2)->nullable()->after('start_ovality');
-        });
-    }
+    //         // Add new column
+    //         $table->decimal('end_ovality', 8, 2)->nullable()->after('start_ovality');
+    //     });
+    // }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::table('finished_goods', function (Blueprint $table) {
-             // Rollback changes
-            $table->renameColumn('start_ovality', 'ovality');
-            $table->dropColumn('end_ovality');
-        });
-    }
+    // public function down(): void
+    // {
+    //     Schema::table('finished_goods', function (Blueprint $table) {
+    //          // Rollback changes
+    //         $table->renameColumn('start_ovality', 'ovality');
+    //         $table->dropColumn('end_ovality');
+    //     });
+    // }
 };
