6db9e748240117c549ebf7073b650cb9adccd4c9
Muhammad Ayaz <fazlani362@gmail.com>
2026-07-23 20:25:28 +0500

    mobile dummy logo removed

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -639,7 +639,6 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
                     </svg>
                 </button>
-                <span class="font-bold bx-text-gradient text-sm">YourApp</span>
                 <div class="w-8"></div>
             </div>
 
